f5406bc2da822e069ece9e8c5cc02195a7b7cd43
John Ansel Doton <johnanseldoton@gmail.com>
2025-07-29 10:11:42 +0800

    hero design v4 (change button design)

--- ui/hero.js
+++ ui/hero.js
@@ -7,7 +7,27 @@ export function loadHero(onContinue) {
     <div class="hero">
       <h1 class="hero__title">Welcome to Deskkk</h1>
       <p class="hero__subtitle">Organize. Learn. Explore.</p>
-      <button class="hero__cta-btn" id="hero-cta">Get Started</button>
+
+      <!-- From Uiverse.io by adamgiebl --> 
+      <button class="uiverse" id="hero-cta">
+        <div class="wrapper">
+          <span>GET STARTED</span>
+          <div class="circle circle-12"></div>
+          <div class="circle circle-11"></div>
+          <div class="circle circle-10"></div>
+          <div class="circle circle-9"></div>
+          <div class="circle circle-8"></div>
+          <div class="circle circle-7"></div>
+          <div class="circle circle-6"></div>
+          <div class="circle circle-5"></div>
+          <div class="circle circle-4"></div>
+          <div class="circle circle-3"></div>
+          <div class="circle circle-2"></div>
+          <div class="circle circle-1"></div>
+        </div>
+      </button>
+
+
     </div>
   `;
 
