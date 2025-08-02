87dd6cab83fea434aeb874c37d028e215014671d
John Ansel Doton <johnanseldoton@gmail.com>
2025-08-02 18:40:25 +0800

    auth v4 (add title and description for page1)

--- ui/auth.js
+++ ui/auth.js
@@ -7,11 +7,31 @@ export function loadAuth(onContinue) {
   <div class="auth">
 
     <div class="auth__page1">
-      
+      <div class="auth__page1--heading">
+        <p class="auth__page1--title">Sit with us today!</p>
+      </div>
+
+      <div class="auth__page1--body">
+        <p class="auth__page1--desc">
+          Join a growing community of focused learners. 
+          Organize your tasks, track your progress, 
+          and make your desk your power zone. Your 
+          smarter student life starts here.
+        </p>
+      </div>
+
+      <div class="auth__page1--turn">
+        <p>already have an account?</p>
+        <a href="">login</a>
+      </div>
     </div>
 
     <div class="auth__page2">
-      <button id="auth-next" class="auth__btn">Continue</button>
+      <div></div>
+      <div></div>
+      <div>
+        <button id="auth-next" class="auth__btn">Continue</button>
+      </div>
     </div>
 
   </div>
