416ee357bc924f9f54616ed0907c5cd751da0495
John Ansel Doton <johnanseldoton@gmail.com>
2025-08-02 21:28:36 +0800

    auth v5 (done page1, page2 started with uiverse import)

--- ui/auth.js
+++ ui/auth.js
@@ -6,13 +6,13 @@ export function loadAuth(onContinue) {
 
   <div class="auth">
 
-    <div class="auth__page1">
-      <div class="auth__page1--heading">
-        <p class="auth__page1--title">Sit with us today!</p>
+    <div class="page1__card">
+      <div class="page1__portion--heading">
+        <p class="page1__paragraph--title">Sit with us today!</p>
       </div>
 
-      <div class="auth__page1--body">
-        <p class="auth__page1--desc">
+      <div class="page1__portion--body">
+        <p class="page1__paragraph--desc">
           Join a growing community of focused learners. 
           Organize your tasks, track your progress, 
           and make your desk your power zone. Your 
@@ -20,17 +20,28 @@ export function loadAuth(onContinue) {
         </p>
       </div>
 
-      <div class="auth__page1--turn">
-        <p>already have an account?</p>
-        <a href="">login</a>
+      <div class="page1__portion--turn">
+        <p class="page1__paragraph--label">already have an account?</p>
+        <a href="" class="page1__link--login">login</a>
       </div>
     </div>
 
-    <div class="auth__page2">
-      <div></div>
-      <div></div>
-      <div>
-        <button id="auth-next" class="auth__btn">Continue</button>
+    <div class="page2__card">
+      <div class="page2__container">
+
+        <div class="form-box">
+          <form class="form">
+              <span class="title">Sign up</span>
+              <span class="subtitle">Create a free account with your email.</span>
+              <div class="form-container">
+                <input type="text" class="input" placeholder="Full Name">
+                <input type="email" class="input" placeholder="Email">
+                <input type="password" class="input" placeholder="Password">
+              </div>
+              <button id="auth-next">Sign up</button>
+          </form>
+        </div>
+        
       </div>
     </div>
 
