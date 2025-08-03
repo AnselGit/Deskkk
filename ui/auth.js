ed9d6d9361670878dfc9f5a12ae82dded0ede63f
John Ansel Doton <johnanseldoton@gmail.com>
2025-08-03 15:51:35 +0800

    auth v6 (done page sign in design)

--- ui/auth.js
+++ ui/auth.js
@@ -22,27 +22,24 @@ export function loadAuth(onContinue) {
 
       <div class="page1__portion--turn">
         <p class="page1__paragraph--label">already have an account?</p>
-        <a href="" class="page1__link--login">login</a>
+        <a href="" class="page1__link--login">Sign In</a>
       </div>
     </div>
 
     <div class="page2__card">
-      <div class="page2__container">
-
-        <div class="form-box">
-          <form class="form">
-              <span class="title">Sign up</span>
-              <span class="subtitle">Create a free account with your email.</span>
-              <div class="form-container">
-                <input type="text" class="input" placeholder="Full Name">
-                <input type="email" class="input" placeholder="Email">
-                <input type="password" class="input" placeholder="Password">
-              </div>
-              <button id="auth-next">Sign up</button>
-          </form>
-        </div>
+      
+      <form class="page2__form--box">
+          <span class="title">Sign up</span>
+          <span class="subtitle">Create a free account with your email.</span>
+          <div class="form-container">
+            <input type="text" class="input" placeholder="Full Name">
+            <input type="email" class="input" placeholder="Email">
+            <input type="password" class="input" placeholder="Password">
+            <input type="password" class="input" placeholder="Confirm">
+          </div>
+          <button class="page2__btn--sign" id="auth-next">Sign up</button>
+      </form>
         
-      </div>
     </div>
 
   </div>
