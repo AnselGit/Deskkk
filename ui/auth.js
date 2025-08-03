0e6a899eb1cfdb1325333970ef9f234fcf97b0c6
John Ansel Doton <johnanseldoton@gmail.com>
2025-08-03 16:28:12 +0800

    auth v7 (done auth design)

--- ui/auth.js
+++ ui/auth.js
@@ -21,14 +21,14 @@ export function loadAuth(onContinue) {
       </div>
 
       <div class="page1__portion--turn">
-        <p class="page1__paragraph--label">already have an account?</p>
-        <a href="" class="page1__link--login">Sign In</a>
+        
       </div>
     </div>
 
     <div class="page2__card">
-      
-      <form class="page2__form--box">
+      <div class="page2__inner">
+
+        <form class="page2__form--box page2__form--signup">
           <span class="title">Sign up</span>
           <span class="subtitle">Create a free account with your email.</span>
           <div class="form-container">
@@ -37,15 +37,49 @@ export function loadAuth(onContinue) {
             <input type="password" class="input" placeholder="Password">
             <input type="password" class="input" placeholder="Confirm">
           </div>
+
+          <div class="page2__ScrollDown">
+            <p class="page2__paragraph--label">already have an account?</p>
+            <a href="" class="page2__link--login" id="scrollDown">Sign in</a>
+          </div>
+          
           <button class="page2__btn--sign" id="auth-next">Sign up</button>
-      </form>
-        
+        </form>
+
+        <form class="page2__form--box page2__form--signin">
+          <span class="title">Sign in</span>
+          <span class="subtitle">Welcome back. Login below.</span>
+          <div class="form-container">
+            <input type="email" class="input" placeholder="Email">
+            <input type="password" class="input" placeholder="Password">
+          </div>
+
+          <div class="page2__ScrollUp">
+            <p class="page2__paragraph--label">Don't have an account?</p>
+            <a href="" class="page2__link--login" id="scrollUp">Sign up</a>
+          </div>
+
+          <button class="page2__btn--sign">Sign in</button>
+        </form>
+      </div>
     </div>
 
   </div>
 `;
 
   document.body.appendChild(div);
+
+  const page2 = div.querySelector('.page2__card');
+
+  div.querySelector('#scrollDown').addEventListener('click', (e) => {
+    e.preventDefault();
+    page2.classList.add('scroll-down');
+  });
+
+  div.querySelector('#scrollUp').addEventListener('click', (e) => {
+    e.preventDefault();
+    page2.classList.remove('scroll-down');
+  });
 
   document.getElementById('auth-next').addEventListener('click', () => {
     onContinue(div);
